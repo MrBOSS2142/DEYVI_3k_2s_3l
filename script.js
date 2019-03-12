fc30612f43238d1179a84ca6d9f4f9c742d573ab
Sukharevich Sergey <mr.boss2142bf@gmail.com>
2019-03-12 12:28:45 +0300

    final

--- script.js
+++ script.js
@@ -1,127 +1,36 @@
 $(document).ready(function() {
-  $("#img_box1").hide();
-  $("#img_box2").hide();
-  $("#img_box3").hide();
-  $("#img_box4").hide();
-  $("#img_box5").hide();
+  $(".img_box").hide();
 
-  $("#cat1").mouseenter(function() {
-    $("#cat1").hide();
-    $("#img_box1").show();
-    $("#img_box1").click(function() {
-      $("#mouse_img1").css({
-        transition: "transform 2s",
-        transform: "rotate(360deg)"
-      });
-      $("#img_box1").css({
-        transition: "transform 2s",
-        "transition-delay": "2s",
-        transform: "translateX(1700px)"
-      });
-
-      function cat() {
-        $("#cat1").show();
-        $("#img_box1").removeAttr("style");
-        $("#mouse_img1").removeAttr("style");
-        $("#img_box1").hide();
-      }
-      setTimeout(cat, 3000);
-    });
+  $(".box").mouseenter(function() {
+    $(this)
+      .find(".cat")
+      .hide();
+    $(this)
+      .find(".img_box")
+      .show();
   });
-
-  $("#cat2").mouseenter(function() {
-    $("#cat2").hide();
-    $("#img_box2").show();
-    $("#img_box2").click(function() {
-      $("#mouse_img2").css({
+  $(".box").click(function() {
+    $(this)
+      .find(".img_box")
+      .find(".mouse_img")
+      .css({
         transition: "transform 2s",
         transform: "rotate(360deg)"
       });
-      $("#img_box2").css({
+    $(this)
+      .find(".img_box")
+      .css({
         transition: "transform 2s",
         "transition-delay": "2s",
         transform: "translateX(1700px)"
       });
 
-      function cat() {
-        $("#cat2").show();
-        $("#img_box2").removeAttr("style");
-        $("#mouse_img2").removeAttr("style");
-        $("#img_box1").hide();
-      }
-      setTimeout(cat, 3000);
-    });
-  });
-
-  $("#cat3").mouseenter(function() {
-    $("#cat3").hide();
-    $("#img_box3").show();
-    $("#img_box3").click(function() {
-      $("#mouse_img3").css({
-        transition: "transform 2s",
-        transform: "rotate(360deg)"
-      });
-      $("#img_box3").css({
-        transition: "transform 2s",
-        "transition-delay": "2s",
-        transform: "translateX(1700px)"
-      });
-
-      function cat() {
-        $("#cat3").show();
-        $("#img_box3").removeAttr("style");
-        $("#mouse_img3").removeAttr("style");
-        $("#img_box3").hide();
-      }
-      setTimeout(cat, 3000);
-    });
-  });
-
-  $("#cat4").mouseenter(function() {
-    $("#cat4").hide();
-    $("#img_box4").show();
-    $("#img_box4").click(function() {
-      $("#mouse_img4").css({
-        transition: "transform 2s",
-        transform: "rotate(360deg)"
-      });
-      $("#img_box4").css({
-        transition: "transform 2s",
-        "transition-delay": "2s",
-        transform: "translateX(1700px)"
-      });
-
-      function cat() {
-        $("#cat4").show();
-        $("#img_box4").removeAttr("style");
-        $("#mouse_img4").removeAttr("style");
-        $("#img_box4").hide();
-      }
-      setTimeout(cat, 3000);
-    });
-  });
-
-  $("#cat5").mouseenter(function() {
-    $("#cat5").hide();
-    $("#img_box5").show();
-    $("#img_box5").click(function() {
-      $("#mouse_img5").css({
-        transition: "transform 2s",
-        transform: "rotate(360deg)"
-      });
-      $("#img_box5").css({
-        transition: "transform 2s",
-        "transition-delay": "2s",
-        transform: "translateX(1700px)"
-      });
-
-      function cat() {
-        $("#cat5").show();
-        $("#img_box5").removeAttr("style");
-        $("#mouse_img5").removeAttr("style");
-        $("#img_box5").hide();
-      }
-      setTimeout(cat, 3000);
-    });
+    setTimeout(cat, 3000);
+    function cat() {
+      $(".cat").show();
+      $(".img_box").removeAttr("style");
+      $(".mouse_img").removeAttr("style");
+      $(".img_box").hide();
+    }
   });
 });
